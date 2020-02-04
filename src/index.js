2758f00bc2c088977b3de051b9db5b37181c8489
zhufeng <zhufeng@youzan.com>
2020-02-04 10:13:28 +0800

    temp

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
+import materialApi from 'js/api/material';
+import {Notify} from 'zent';
+import fullfillImage from 'zan-utils/fullfillImage';
+
 import "./style.scss";
+import Axios from 'axios';
 
 export default class Upload extends Component {
   constructor(prop) {
@@ -18,7 +23,7 @@ export default class Upload extends Component {
         <img
           width={80}
           height={80}
-          src={url}
+          src={fullfillImage(url)}
           className={"preview-upload-pic"}
         />
         <span className="delete-upload-pic" onClick={() => this.delete(key)}>
@@ -37,66 +42,92 @@ export default class Upload extends Component {
   }
 
   uploadFile = e => {
-    const files = e.target.files;
-    let index = 0;
-    const upload = i => {
-      const formData = new FormData();
-      const file = files[i];
-      formData.append("file", file);
-
-      // promise
-      const { value, onChange, uploadPromise, maxAmount } = this.props;
-      uploadPromise(formData)
-        .then(res2 => {
-          const arr = value;
-          arr.push(res2);
-          onChange(arr);
-        })
-        .catch(e => console.log(e));
-
-      index++;
-      if (index < files.length) {
-        upload(index);
-      }
-    };
-
-    upload(index);
-  };
-
-  render() {
-    const { maxAmount = 1, className, accept = "*/*", value } = this.props;
     let { maxSize } = this.props;
     if (!maxSize) {
       maxSize = 3 * 1024 * 1024;
     }
+    const { value, onChange, maxAmount = 1 } = this.props;
+    const files = e.target.files;
+
+    if(files.length < 1){
+      return;
+    }
+
+    materialApi.getPublicUploadToken().then(res=>{
+      
+      const limit = Math.min(maxAmount - value.length, files.length);
+      let index = 0;
+
+      const upload = i => {
+        const formData = new FormData();
+        const file = files[i];
+        formData.append("token", res);
+        formData.append("file", file);
+        Axios({
+          baseURL: 'https://upload.qbox.me',
+          method: 'post',
+          url: '/',
+          data: formData
+        })
+        .then(res2 => {
+          const obj = res2.data.data;
+          if(obj.attachment_size <= maxSize){
+            value.push(res2.data.data);
+            onChange(value);
+          }else{
+            Notify.error(`图片${file.name}大小超过限制`);
+          }
+        })
+        .catch(e=>{
+          Notify.error(e)
+        }).then(()=>{
+          index++;
+          if (index < limit) {
+            upload(index);
+          }
+        });
+      };
+
+      upload(index);
+    }).catch(e=>{
+      Notify.error(e)
+    });
+  };
+
+  render() {
+    const { maxAmount = 1, uploadClassName, tipsClassName, accept = "*/*", value, tips } = this.props;
+    
     return (
-      <div className={className}>
-        {value.length > 0 && (
-          <div className="ecloud-comp-upload-preview">
-            {this.renderImg(value)}
-          </div>
-        )}
-        {value.length < maxAmount && (
-          <div
-            className="ecloud-comp-upload-upload"
-            onClick={e => {
-              if (e.target.childNodes && e.target.childNodes.length > 1) {
-                e.target.childNodes[1].click();
-              }
-            }}
-          >
-            +
-            <input
-              width="80"
-              height="80"
-              type="file"
-              multiple={maxAmount > 1}
-              accept={accept}
-              onChange={this.uploadFile}
-            />
-          </div>
-        )}
-      </div>
+      <React.Fragment>
+        <div className={uploadClassName}>
+          {value.length > 0 && this.renderImg(value)}
+          {value.length < maxAmount && (
+            <div
+              className="ecloud-comp-upload-upload"
+              onClick={e => {
+                if (e.target.childNodes && e.target.childNodes.length > 1) {
+                  e.target.childNodes[1].value = null;
+                  e.target.childNodes[1].click();
+                }
+              }}
+            >
+              +
+              <input
+                width="80"
+                height="80"
+                type="file"
+                multiple={maxAmount > 1}
+                accept={accept}
+                onChange={this.uploadFile}
+              />
+            </div>
+          )}
+        </div>
+        <div className={`ecloud-comp-upload-tips ${tipsClassName}`}>
+          {tips}
+        </div>
+      </React.Fragment>
+      
     );
   }
 }
